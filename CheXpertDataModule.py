17c570e5a4b1aaa40308a8d326b5f9b91230a596
Satyam Ghodasara <sghodas@gmail.com>
2023-10-23 14:07:37 -0400

    Add CheXpertDataModule implementation

---
+++ CheXpertDataModule.py
@@ -0,0 +1,57 @@
+import os
+import lightning.pytorch as pl
+import torch
+import torch.utils.data as data
+import torchvision.transforms as T
+import CheXpertDataset
+
+
+class CheXpertDataModule(pl.LightningDataModule):
+    def __init__(self, tasks, batch_size=64, data_path="../"):
+        super().__init__()
+        self.batch_size = batch_size
+        self.data_path = data_path
+        self.tasks = tasks
+
+        # Create the DataFrame paths
+        self.train_df_path = os.path.join(
+            self.data_path, "CheXpert-v1.0-small", "train.csv"
+        )
+        self.val_df_path = os.path.join(
+            self.data_path, "CheXpert-v1.0-small", "valid.csv"
+        )
+
+        # Setup the transforms
+        self.transform = None
+        self.target_transform = None
+
+    def train_dataloader(self):
+        train_dataset = CheXpertDataset(
+            self.data_path,
+            self.train_df_path,
+            self.tasks,
+            self.transform,
+            self.target_transform,
+        )
+        return data.DataLoader(
+            train_dataset,
+            batch_size=self.batch_size,
+            num_workers=10,
+            shuffle=True,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self):
+        val_dataset = CheXpertDataset(
+            self.data_path,
+            self.val_df_path,
+            self.tasks,
+            self.transform,
+            self.target_transform,
+        )
+        return data.DataLoader(
+            val_dataset,
+            batch_size=self.batch_size,
+            num_workers=10,
+            shuffle=False,
+        )
