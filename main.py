4756f918638f84b8a427de614ad8788e992d0759
Satyam Ghodasara <sghodas@gmail.com>
2023-10-26 14:41:28 -0400

    Implement Lightning CLI

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from CheXpertModule import CheXpertModule
 from CheXpertDataModule import CheXpertDataModule
-import lightning.pytorch as pl
+from lightning.pytorch.cli import LightningCLI
 
 
 def main():
-    tasks = [
-        "Atelectasis",
-        "Cardiomegaly",
-        "Consolidation",
-        "Edema",
-        "Pleural Effusion",
-    ]
-    model = CheXpertModule(tasks, 1e-4)
-    data = CheXpertDataModule(tasks)
-    trainer = pl.Trainer(max_epochs=3)
-    trainer.fit(model, datamodule=data)
+    LightningCLI(CheXpertModule, CheXpertDataModule)
 
 
 if __name__ == "__main__":
